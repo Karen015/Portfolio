07c82bcee2a28eef119f78bf9ed1a622b16ff9c6
Karen015 <karen.aghajanyan.11@gmail.com>
2025-08-27 14:48:23 +0400

    change shop to list am

--- src/onlineShopSection/index.js
+++ src/onlineShopSection/index.js
@@ -3,18 +3,18 @@ import './index.css';
 const OnlineShopSection = () => {
   return (
     <section className="project-section" id="onlineShop">
-        <h2>Online Shop</h2>
+        <h2>List Am Clone</h2>
         <ul className="listed">
             <li>Product catalog with filtering</li>
             <li>Dynamic rendering of different product categories</li>
             <li>Clean and interactive UI</li>
         </ul>
 
-        <p><strong>Technologies Used:</strong> HTML, CSS, LESS, JS.</p>
+        <p><strong>Technologies Used:</strong> HTML, CSS, JS, React, Meilisearch, React-Router</p>
 
-        <a href="https://elegant-cheesecake-af968f.netlify.app/" target="_blank" rel="noreferrer">View Project</a>
+        <a href="https://super-licorice-5c8f8c.netlify.app/" target="_blank" rel="noreferrer">View Project</a>
         <div className="project-preview">
-           <img src="/images/shop.png" alt="Online Shop "></img>
+           <img src="/images/listAm.png" alt="Online Shop "></img>
         </div>
     </section>
   );
